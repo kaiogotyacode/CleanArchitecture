f568228a989bc825f53da7df533b866f2566305c
Kaio Maciel <kaioreis1153@hotmail.com>
2023-12-15 16:54:06 -0300

    feat: Added Base Entity | Interfaces (Base Repositories)

---
+++ CleanArchitecture.Domain/Entities/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public abstract class BaseEntity
+    {
+        public BaseEntity()
+        {
+            DateCreated = DateTime.Now;    
+        }
+
+        public Guid Id { get; set; }
+        public DateTimeOffset DateCreated { get; set; }
+        public DateTimeOffset DateUpdated { get; set; }
+        public DateTimeOffset DateDeleted { get; set; }
+    }
+}
