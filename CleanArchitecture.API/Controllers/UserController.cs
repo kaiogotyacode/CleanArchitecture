9d9e72cdaa8d1c585ab40a037b7b22e4e17424c3
Kaio Maciel <kaioreis1153@hotmail.com>
2023-12-16 20:49:28 -0300

    feat: Created POST method

---
+++ CleanArchitecture.API/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using CleanArchitecture.Application.UseCases.CreateUser;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitecture.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("Create")]
+        public async Task<ActionResult<CreateUserResponse>> Create(CreateUserRequest request, CancellationToken cancellationToken)
+        {
+            var response = await _mediator.Send(request, cancellationToken);
+
+            return Ok(response);
+        }
+
+    }
+}
